0f8e071ddf6841f089b412be2d982f2b8fa63706
LahiruHarshana <lharshana2002@gmail.com>
2024-01-26 09:18:48 +0530

    updated

--- src/main/java/lk/ijse/inclasspractise/dbconnectionpool/HelloServlet.java
+++ src/main/java/lk/ijse/inclasspractise/dbconnectionpool/HelloServlet.java
@@ -14,13 +14,6 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
     }
 
     public void destroy() {
