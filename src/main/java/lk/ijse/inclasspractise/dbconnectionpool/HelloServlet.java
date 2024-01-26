b844691652ce0c61661267b0aa150b97e5770a6c
LahiruHarshana <lharshana2002@gmail.com>
2024-01-26 09:53:04 +0530

    updated

--- src/main/java/lk/ijse/inclasspractise/dbconnectionpool/HelloServlet.java
+++ src/main/java/lk/ijse/inclasspractise/dbconnectionpool/HelloServlet.java
@@ -1,9 +1,16 @@
 package lk.ijse.inclasspractise.dbconnectionpool;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @WebServlet(name = "helloServlet", value = "/servlet")
 public class HelloServlet extends HttpServlet {
@@ -13,6 +20,29 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Context initContext = null;
+        try {
+            initContext = new InitialContext();
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+        Context envContext  = null;
+        try {
+            envContext = (Context)initContext.lookup("java:/comp/env");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+        DataSource ds = null;
+        try {
+            ds = (DataSource)envContext.lookup("jdbc/myoracle");
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Connection conn = ds.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
